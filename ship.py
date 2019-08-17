63c970c728690854e7be1f44a65c14326fe45abb
CodeLarva <45579597+CodeLarva@users.noreply.github.com>
2019-08-17 14:39:10 +0800

    Initial commit

---
+++ ship.py
@@ -0,0 +1,28 @@
+import pygame
+class Ship():
+
+    def __init__(self,screen):
+        """初始化飞船并设置其位置"""
+        self.screen=screen
+
+        # 加载飞船图像并获取其外界矩形
+        self.image = pygame.image.load('image/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect=screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # 移动标志
+        self.moving_right=False
+        self.moving_left=False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
+
+    def blitme(self):
+        """指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
